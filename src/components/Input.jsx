8ad3cd7c94d43037c030611993c09f502dff4387
apple <apple@apples-MacBook-Pro.local>
2024-12-05 01:49:21 +0530

    Added validation

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -7,14 +7,14 @@ export const Input = () => {
     return (
         <div>
             <h1>Full Name Display</h1>
-            <form action="">
+            <form onSubmit={(e) => {e.preventDefault(); fname && lname && setSubmitted(true);}}>
                 <label htmlFor="fn">First Name:</label>
                 <input type="text" id="fn" required onChange={(e) => setFname(e.target.value)} />
                 <br />
                 <label htmlFor="ln">Last Name:</label>
                 <input type="text" id="ln" required onChange={(e) => setLname(e.target.value)}/>
                 <br />
-                <button onClick={(e) => {fname && lname && setSubmitted(true); e.preventDefault()}}>Submit</button>
+                <button type='submit'>Submit</button>
             </form>
             {isSubmitted && fname && lname && `Full Name: ${fname} ${lname}`}
         </div>
